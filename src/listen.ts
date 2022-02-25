a8b5013117d75048376a1adb06ad9bd1eb720fb2
Joseph Dunn <josephandrewdunn@gmail.com>
2022-02-25 11:15:01 -0600

    Further work on getting CPU-intensive stuff out of main process and into MD listeners

--- src/listen.ts
+++ src/listen.ts
@@ -1,12 +1,86 @@
+import fs from 'fs';
+import path from 'path';
 import {
   normalizeBookChanges,
   normalizeTrades,
   streamNormalized,
+  OrderBook,
 } from 'tardis-dev';
 
-async function listenFtxBooks() {
+class TardisBook extends OrderBook {
+  getQuoteSizedBestBid(quoteSize: number): number | undefined {
+    let rem = quoteSize;
+    for (const bid of this.bids()) {
+      rem -= bid.amount * bid.price;
+      if (rem <= 0) {
+        return bid.price;
+      }
+    }
+    return undefined;
+  }
+  getQuoteSizedBestAsk(quoteSize: number): number | undefined {
+    let rem = quoteSize;
+    for (const ask of this.asks()) {
+      rem -= ask.amount * ask.price;
+      if (rem <= 0) {
+        return ask.price;
+      }
+    }
+    return undefined;
+  }
+  getMid(): number | undefined {
+    const b = this.bestBid();
+    const a = this.bestAsk();
+    return a && b ? (a.price + b.price) / 2 : undefined;
+  }
+}
+
+type MultiBook = {
+  marketName: string;
+  books: {
+    ftx: { book: TardisBook; updateTime: number; };
+    binance: { book: TardisBook, updateTime: number; };
+  };
+};
+
+type EquityMessage = {
+  equity: number;
+};
+
+const paramsFileName = process.env.PARAMS || 'default.json';
+const params = JSON.parse(
+  fs.readFileSync(
+    path.resolve(__dirname, `../params/${paramsFileName}`),
+    'utf-8',
+  ),
+);
+
+function listMin(list: (number | undefined)[]): number | undefined {
+  list = list.filter((e) => e !== undefined);
+  if (list.length === 0) return undefined;
+  // @ts-ignore
+  return Math.min(...list);
+}
+
+function listMax(list: (number | undefined)[]): number | undefined {
+  list = list.filter((e) => e !== undefined);
+  if (list.length === 0) return undefined;
+  // @ts-ignore
+  return Math.max(...list);
+}
+
+let equity: number = 0;
+process.on('message', (m: EquityMessage) => {
+  equity = m.equity;
+});
+let ftxMid: number = 0;
+
+async function listenFtxBooks(books: MultiBook[]) {
   if (process.send === undefined) throw new Error('process.send is undefined');
-  const marketNames = process.argv[2].split(',');
+  const marketNames = books.map((book) => book.marketName);
+  const marketNameToBook = Object.fromEntries(
+    books.map((book) => [book.marketName, book.books]),
+  );
 
   const messages = streamNormalized(
     {
@@ -19,15 +93,39 @@ async function listenFtxBooks() {
 
   for await (const msg of messages) {
     if (msg.type === 'book_change') {
-      process.send({ bookUpdate:      msg,
-                     nativeExchange:  'ftx',
-                     nativeMarket:    msg.symbol,
-                     nativeTimestamp: msg.timestamp.getTime() / 1000 });
+      const marketName = msg.symbol;
+      const book = marketNameToBook[marketName].ftx;
+      book.book.update(msg);
+      book.updateTime = msg.timestamp.getTime() / 1000;
+
+      const assetParams = params.assets[marketName.split('-')[0]].perp;
+      const ftxSize = assetParams.ftxSize;
+      const sizePerc = assetParams.sizePerc;
+      const quoteSize = equity * sizePerc;
+      const aggBid = listMin(
+        Object.values(marketNameToBook[marketName]).map(({ book, updateTime }) =>
+          book.getQuoteSizedBestBid(ftxSize || quoteSize),
+        ),
+      );
+      const aggAsk = listMax(
+        Object.values(marketNameToBook[marketName]).map(({ book, updateTime }) =>
+          book.getQuoteSizedBestAsk(ftxSize || quoteSize),
+        ),
+      );
+      ftxMid = book.book.getMid() || 0;
+      process.send({ marketName: marketName,
+                     aggBid:     aggBid,
+                     aggAsk:     aggAsk,
+                     ftxMid:     ftxMid });
     }
   }
+
+  process.on('message', (m: EquityMessage) => {
+    equity = m.equity;
+  });
 }
 
-async function listenBinanceBooks() {
+async function listenBinanceBooks(books: MultiBook[]) {
   if (process.send === undefined) throw new Error('process.send is undefined');
   const binanceList = [
     'BTCUSDT',
@@ -41,8 +139,11 @@ async function listenBinanceBooks() {
     'ADAUSDT',
   ];
   const systemMarketToNativeMarket = function(systemMarket: string) { return `${systemMarket.split('-')[0]}USDT` };
-  const marketNames = process.argv[2].split(',').filter((mn) =>
+  const marketNames = books.map((book) => book.marketName).filter((mn) =>
     binanceList.includes(systemMarketToNativeMarket(mn)),
+  );
+  const marketNameToBook = Object.fromEntries(
+    books.map((book) => [book.marketName, book.books]),
   );
   const nativeMarketMap = Object.fromEntries(marketNames.map((mn) => [
     systemMarketToNativeMarket(mn),
@@ -60,13 +161,43 @@ async function listenBinanceBooks() {
 
   for await (const msg of messages) {
     if (msg.type === 'book_change') {
-      process.send({ bookUpdate:      msg,
-                     nativeExchange:  'binance',
-                     nativeMarket:    nativeMarketMap[msg.symbol],
-                     nativeTimestamp: msg.timestamp.getTime() / 1000 });
+      const marketName = nativeMarketMap[msg.symbol];
+      const book = marketNameToBook[marketName].binance;
+      book.book.update(msg);
+      book.updateTime = msg.timestamp.getTime() / 1000;
+
+      const assetParams = params.assets[marketName.split('-')[0]].perp;
+      const ftxSize = assetParams.ftxSize;
+      const sizePerc = assetParams.sizePerc;
+      const quoteSize = equity * sizePerc;
+      const aggBid = listMin(
+        Object.values(marketNameToBook[marketName]).map(({ book, updateTime }) =>
+          book.getQuoteSizedBestBid(ftxSize || quoteSize),
+        ),
+      );
+      const aggAsk = listMax(
+        Object.values(marketNameToBook[marketName]).map(({ book, updateTime }) =>
+          book.getQuoteSizedBestAsk(ftxSize || quoteSize),
+        ),
+      );
+      process.send({ marketName: marketName,
+                     aggBid:     aggBid,
+                     aggAsk:     aggAsk,
+                     ftxMid:     ftxMid });
     }
   }
 }
 
-listenFtxBooks();
-listenBinanceBooks();
+const marketNames = process.argv[2].split(',');
+const books: MultiBook[] = marketNames.map((mn) => {
+    return {
+      marketName: mn,
+      books: {
+        ftx: { book: new TardisBook(), updateTime: 0 },
+        binance: { book: new TardisBook(), updateTime: 0 },
+      },
+    };
+});
+
+listenFtxBooks(books);
+listenBinanceBooks(books);
